29ee72c7717168c40027efca0b979cafaba204a9
Jason Vu <jvu@scu.edu>
2022-06-22 11:19:21 -0700

    else: pass (for albums without covers)

--- lastfm.py
+++ lastfm.py
@@ -36,6 +36,8 @@ def get_album_covers(artist_and_album):
         link_to_album = request_response["album"]["url"]
         if (url != ""):
             images.append([album[0], album[1], url, link_to_album]) 
+        else:
+            pass
     return images
 
 def get_avg_img_color(image):
