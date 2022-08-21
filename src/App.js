f77c79021f3f517e2d7553db8ba6df8bd83e2c4c
Jason Vu <jvu@scu.edu>
2022-08-20 22:40:41 -0700

    adjust code syntactically

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React, { Component } from "react";
-import './App.css';    
-import Header from './components/Header';
+import React from "react";
+import './App.css';     
+import NavBar from './components/NavBar';
 import HomeScreen from './components/HomeScreen';
 import AboutMe from './components/AboutMe' ;
 import Projects from './components/Projects';
 import Skills from './components/Skills' ;
-import Footer from './components/Footer' ;
+import Footer from './components/Footer' ;   
  
-export default class App extends Component {     
-  render () {
+function App() {       
     return (  
-      <div className="App">    
-        <Header />
-        <HomeScreen />
-        <AboutMe />
-        <Projects />
-        <Skills />
-        <Footer /> 
+      <div>
+        <div>
+          <NavBar />
+        </div>
+        <div>
+          <HomeScreen />
+        </div>
+        <div>
+          <AboutMe />
+        </div>
+        <div> 
+          <Projects />
+        </div>
+        <div>
+          <Skills />
+        </div>
+        <div>
+          <Footer />
+        </div> 
       </div>
-    ); 
-  }
+    );  
 } 
+
+export default App
---
+++ src/components/Resume.js
@@ -0,0 +1,15 @@
+/*
+
+import testPdf from '../jason-vu-resume.pdf'
+
+function Resume() {
+    return ( 
+        <div>
+            <iframe src={`${testPdf}#view=fitH`} title="Jason Vu's Resume" height="100%" width="100%" frameBorder="0"></iframe>
+        </div>
+    )
+}
+
+export default Resume;
+
+*/
